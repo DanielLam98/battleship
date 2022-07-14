d49f2b1bb756d8ecf001c7db9af90fb907978d35
Daniel <lamdaniel@hotmail.ca>
2022-07-13 23:28:20 -0700

    styling changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Player from './player';
 import Ship from './ships';
-import html from './index.html';
 import './style.css';
 import './normalize.css';
 
@@ -24,12 +23,14 @@ const main = () => {
   ]);
   displayGameboard('.playerGameboard');
   displayGameboard('.computerGameboard');
+  displayShips('.playerGameboard', player.ships);
+  //displayShips('.computerGameboard', player.ships);
+  selectSpot('.computerGameboardContainer');
 };
 
 const placePlayerShips = (ships) => {
   for (let i = 0; i < 5; i++) {
     if (player.gameboard.placeShip(ships[i].value) === true) {
-      //playerShips[i] = Ship(ships[i].value);
       player.ships.push(Ship(ships[i].value));
     }
   }
@@ -58,6 +59,44 @@ const displayGameboard = (gameboard) => {
     }
   };
   createGameboard();
+};
+
+const displayShips = (gameboard, ships) => {
+  const tableGameboard = document
+    .querySelector(`${gameboard}`)
+    .querySelectorAll('td');
+  for (const number of ships) {
+    number.position.forEach((element) => {
+      tableGameboard[element - 1].classList.add('ships');
+    });
+  }
+};
+
+const playGame = () => {
+  let playerShipArr = [];
+  let computerShipArr = [];
+  //Place player ship function here
+  for (let i = 0; i < 5; i++) {
+    playerShipArr.push(...player.ships[i].position);
+    computerShipArr.push(...computer.ships[i].position);
+  }
+  while (
+    !player.gameboard.allShipsSunk(playerShipArr) ||
+    !computer.gameboard.allShipsSunk(computerShipArr)
+  ) {
+    //player selects a tile on computer board
+  }
+};
+
+const selectSpot = (gameboard, player, opponentBoard) => {
+  let response = true;
+  while (response) {
+    const gameboardTable = document
+      .querySelector(`${gameboard}`)
+      .querySelectorAll('td');
+    console.log(gameboardTable);
+    response = false;
+  }
 };
 
 main();
